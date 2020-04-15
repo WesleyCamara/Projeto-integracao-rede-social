beeef3edba52fffdc16a69fad8ba26eb5f377107
WesleyCamara <wesleycamara@hotmail.com.br>
2020-04-14 23:53:07 -0300

    ajustes na logica do input

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  // @ViewChild ('erroAtivo') erroAtivo:any
+  erroAtivo: any
+  conteudoInput: any
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+   // função de consultar a API, ainda não foi implementada, no momento só previne o envio do formulário
+    pesquisar(event: any) {
+      event.preventDefault();
+      this.verificaConteudoVazio(event);
+    }
 
+    verificaConteudoVazio(event) {
+      if (!this.conteudoInput) {
+        this.erroAtivo = 'erro';
+      } else {
+        this.erroAtivo = '';
+      }
+  }
+
+    // // Valida os dados para pesquisa
+    valida(event){
+      this.erroAtivo = '';
+      const textoVerificar = event.target.value;
+      this.limpaHashtag(textoVerificar);
+    }
+
+
+    // Limpa o texto tirando o caractere # se houver
+    limpaHashtag(texto) {
+        if (texto[0] === '#') {
+          texto = texto.replace(texto[0], '');
+            console.log('valor limpo: ' + texto);
+        }
+    }
+
+}
