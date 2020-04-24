9fa009df2eb32ce10c9c59a1b4b557e067407c7b
Harumi <haruumi.nishimura@gmail.com>
2020-04-23 22:54:06 -0300

    sobre melhorado e funcionando

--- src/app/sobre/sobre.component.ts
+++ src/app/sobre/sobre.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobreComponent implements OnInit {
 
+  pessoa: any = null;
+
+  isMobile = false;
+  getIsMobile(): boolean {
+    const w = document.documentElement.clientWidth;
+    const breakpoint = 1024;
+    console.log(w);
+    if (w <= breakpoint) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   constructor() { }
 
+
   ngOnInit(): void {
+
+    this.isMobile = this.getIsMobile();
+    window.onresize = () => {
+      this.isMobile = this.getIsMobile();
+    };
+
+
+  }
+
+  escolhePessoa(nome){
+    this.pessoa = nome;
   }
 
 }
